f8b74a5504dac66735ea862ac206e7d8435a866b
Hai <hailv@magezon.com>
2024-05-22 11:38:14 +0700

    add logic call api

--- app.js
+++ app.js
@@ -1,11 +1,39 @@
-const express = require("express");
+import express from "express";
+import http from "http";
+import cors from "cors";
+import mysql from "mysql2";
+import bodyParser from "body-parser";
+
 const app = express();
-const port = 3000;
+app.use(cors());
+app.use(bodyParser.json());
+
+const mysqlUrl = `mysql://root:LReXakBPjJUiTPhNEFnNZsMQtgLRFWGD@roundhouse.proxy.rlwy.net:50967/railway`;
+
+// const db = mysql.createConnection({
+// 	host: "roundhouse.proxy.rlwy.net",
+// 	user: "root",
+// 	password: "LReXakBPjJUiTPhNEFnNZsMQtgLRFWGD",
+// 	database: "railway",
+// 	port: 50967,
+// });
+
+const db = mysql.createConnection(mysqlUrl);
+
+const server = http.createServer(app);
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+	res.send("hello world");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.get("/getUser", (req, res) => {
+	db.query("Select * from Infor", (err, result) => {
+		if (result) {
+			res.send(result);
+		}
+	});
+});
+
+server.listen(4555, () => {
+	console.log("Server is running on port");
 });
